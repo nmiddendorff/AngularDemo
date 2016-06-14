165970217c403659c938d7eb39f5b64b28cc0644
ahelgeson <helgesonaustin@gmail.com>
2016-06-13 20:05:02 -0500

    Revert "Added Files"
    This reverts commit 3c14b9ac9861151ea758a6ad903a773ba4b6c398.

--- SPAEmpty/App/Components/About/aboutController.js
+++
@@ -1,5 +0,0 @@
-﻿'use strict';
-
-angular.module('main').controller('aboutController', function ($scope) {
-    $scope.message = "Now viewing about!";
-});
--- SPAEmpty/App/Components/Home/homeController.js
+++
@@ -1,5 +0,0 @@
-﻿'use strict';
-
-angular.module('main').controller('homeController', function ($scope) {
-    $scope.message = "Now viewing home!";
-});
